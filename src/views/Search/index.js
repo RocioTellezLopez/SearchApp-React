63544eb930321dd9390149c2c199fb4ef90d44b0
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-11-18 16:57:05 -0500

    Agrega funcion para handleCloseOpenSearch para cambiar el class de compomente

--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import SearchBox from "./components/SearchBox/index";
 import './style.css';
 
 export default function Search() {
+  const [isAtTop, setIsAtTop] = useState(false); 
+
+  const handleCloseOpenSearch = () => {
+    setIsAtTop(!isAtTop);
+  }
 
   return (
-    <div className='search'>
-      <SearchBox />
+    <div className={`search ${isAtTop? 'search--top' : 'search--center'}`}>
+      <SearchBox onSearch={handleCloseOpenSearch} onClose={handleCloseOpenSearch}/>
     </div>
   );
 }
