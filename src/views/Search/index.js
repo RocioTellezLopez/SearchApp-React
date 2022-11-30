df99dbc47c0dd3e68f0f6bc6d534efe11361b43d
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-11-29 23:40:26 -0500

    Agrega onClick para el componente SearchBox

--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -1,17 +1,37 @@
 import { useState } from "react";
 import SearchBox from "./components/SearchBox/index";
+import data from '../../data/users.json'
+
 import './style.css';
 
 export default function Search() {
   const [isAtTop, setIsAtTop] = useState(false); 
+  const [userData, setUsersData] = useState(data);
+  const [results, setResults] = useState([])
 
-  const handleCloseOpenSearch = () => {
-    setIsAtTop(!isAtTop);
+  const handleCloseSearch = () => {
+    setIsAtTop(false);
+    setResults([]);
   }
 
+  const handleSearchClick = (searchText) => {
+    if(userData?.length) {
+      const searchTextMinus = searchText.toLowerCase();
+      const filteredData = userData.filter((value) => {
+        return (
+          value.name.toLowerCase().includes(searchTextMinus) || 
+          value.phone.toLowerCase().includes(searchTextMinus) ||
+          value.email.toLowerCase().includes(searchTextMinus) ||
+          value.username.toLowerCase().includes(searchTextMinus)
+        );
+      });
+
+      setResults(filteredData);
+    }
+  };
   return (
     <div className={`search ${isAtTop? 'search--top' : 'search--center'}`}>
-      <SearchBox onSearch={handleCloseOpenSearch} onClose={handleCloseOpenSearch}/>
+      <SearchBox onSearch={handleSearchClick} onClose={handleCloseSearch}/>
     </div>
   );
 }
